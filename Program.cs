fd276778040d264b8768be72384f910b24ef797e
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-23 15:45:47 -0300

    feat: Middlewares

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Middleware Redirecting from tasks/{whatever} to todos/{whatever}
+// why would I ever want to redirect, anyway?
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
+
+// Middleware (custom middleware for logging. Classic!)
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
+});
+
 var todos = new List<Todo>();
-
 app.MapGet("/todos", () => todos);
-
 app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
 {
     var targetTodo = todos.SingleOrDefault(t => id == t.Id);
@@ -14,13 +25,11 @@ app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
         ? TypedResults.NotFound()
         : TypedResults.Ok(targetTodo);
 });
-
 app.MapPost("/todos", (Todo task) =>
 {
     todos.Add(task);
     return TypedResults.Created("/todos/{id}", task);
 });
-
 app.MapDelete("/todos/{id}", (int id) =>
 {
     todos.RemoveAll((t => id == t.Id));
